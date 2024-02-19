15f1e5f14ddb6c292c2729dea7dfae21d224cdda
kjirstenhernandez <122486287+kjirstenhernandez@users.noreply.github.com>
2024-02-19 09:52:56 -0700

    Changing Styling

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -10,67 +10,26 @@ let div = buildGrid(data);
 document.querySelector("#carousel").innerHTML = div;
 
 function buildGrid(data) {
-  let grid = `
-  <div class="slideshow-container">
-        <div class="mySlides fade">
-            <div class="numbertext">1 / 3</div>
-            <img src="${data[0].url}">
-            <div class="text">Caption Text</div>
-        </div>
-
-        <div class="mySlides fade">
-            <div class="numbertext">2 / 3</div>
-            <img src="${data[1].url}">
-            <div class="text">Caption Two</div>
-        </div>
-
-        <div class="mySlides fade">
-            <div class="numbertext">3 / 3</div>
-            <img src="${data[2].url}">
-            <div class="text">Caption Three</div>
-        </div>
-    
-        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-        <a class="next" onclick="plusSlides(1)">&#10095;</a>
-    
-    </div>
-    <br>
-    
-    <div style="text-align:center">
-        <span class="dot" onclick="currentSlide(1)"></span>
-        <span class="dot" onclick="currentSlide(2)"></span>
-        <span class="dot" onclick="currentSlide(3)"></span>
-    </div>`;
-
-  //   data.forEach((image) => {
-  //     grid = '<div class="myslides fade">';
-  //     grid += `<img src=${image.url} style="width:100%">`;
-  //     if (image.breed) {
-  //       grid += `<div class="text">${image.breed[0].name}</div>`;
-  //     }
-  //     grid += "</div>";
-  //   };
+  let grid = "";
+  data.forEach((image) => {
+    grid += `<div class="image fade"><img src="${image.url}" alt="Random Cat Image"></div>`;
+  });
   return grid;
 }
 
-let slideIndex = 0;
-showSlides();
+let index = 0;
+displayImages();
 
-function showSlides() {
+function displayImages() {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  const images = document.getElementsByClassName("image");
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
+  index++;
+  if (index > images.length) {
+    index = 1;
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  images[index - 1].style.display = "block";
+  setTimeout(displayImages, 6000);
 }
